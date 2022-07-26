e9fade072ae64daa1f2d45be7f16c7e534b6f758
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-07-26 07:36:30 -0400

    Add files via upload

---
+++ httpturtle.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://docs.python.org/3/library/turtle.html'
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+prelink = 'https://docs.python.org/3/library/turtle.html'
+filename = 'turt.txt'
+
+f=open('C:\\giz\\'+filename, 'w')
+f.write('')
+f.close()
+f=open('C:\\giz\\'+filename, 'a')
+
+urls=[]
+for link in soup.find_all('a'):
+    urls.append(link.get('href'))
+
+urls = set(urls)
+count=0
+for link in urls:
+    count+=1
+    print('grabbing '+str(count)+'\\'+str(len(urls))+'\t'+str(link.translate(link.maketrans('', '', '#'))))
+    biglink = prelink+str(link)
+    reqs=requests.get(biglink)
+    soup=BeautifulSoup(reqs.text, 'html.parser')
+    # f.write(str(soup.get_text())+'\n')
+    page=str(soup.get_text().encode('utf-8'))
+    mytable=page.maketrans('\n', chr(32))
+    f.write('**********************'+link.translate(link.maketrans('', '', '#'))+'**********************\n\n'+page.translate(mytable)+'\n\n')
+f.close()
