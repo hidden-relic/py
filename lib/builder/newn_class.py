f381cd599cc50ff7ca628efdfddaeda401fd4210
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-08-05 07:29:38 -0400

    gui done?

--- lib/builder/newn_class.py
+++ lib/builder/newn_class.py
@@ -15,26 +15,29 @@ class Framing:
 			self.vals["ft"].append(i)
 			if i < 12:
 				self.vals["in"].append(i)
+		# self.myStyle=ttk.Style()
+		# self.myStyle.configure('TNotebook.Tab', padding=[12, 12], font=('Helvetica', 10))
+		# self.myStyle.configure('TNotebook', tabposition='wn')
 		self.buildNotebooks()
 		self.buildRoofTab()
 		self.buildDeckTab()
 
-	def buildMeasurementInput(self, parent, text, ft=False, inch=False, fr=False):
+	def buildMeasurementInput(self, parent, text, ft=True, inch=True, fr=True):
 		o = {"ft": False, "in": False, "fr": False}
 		base_frame=tk.Frame(parent)
-		base_frame.pack()
+		base_frame.pack(ipady=6)
 		lbl_frame=tk.Frame(base_frame)
-		tk.Label(lbl_frame, text=text, font=self.myfont).pack(side='top', padx=12, pady=1)
-		lbl_frame.pack(side='top')
+		tk.Label(lbl_frame, text=text, font=self.myfont).pack(side='top', padx=6, pady=1)
+		lbl_frame.pack(side='top', ipady=6)
 		main_frame = tk.Frame(base_frame)
 
 		def buildMFt(self):
 			ft_frame = tk.Frame(main_frame)
 			ft_frame.grid(column=0, row=0, sticky="w")
 
-			tk.Label(ft_frame, text="ft.", font=self.myfont).pack(padx=6, ipadx=12)
+			tk.Label(ft_frame, text="ft.", font=self.myfont).pack(padx=6, ipadx=6)
 			ft = ttk.Combobox(ft_frame, height=20, width=6, values=tuple(self.vals["ft"]), font=self.myfont, state="readonly")
-			ft.pack(padx=6, pady=12, ipadx=12)
+			ft.pack(padx=6, pady=6, ipadx=6)
 			ft.set(10)
 			return ft
 
@@ -42,9 +45,9 @@ class Framing:
 			in_frame = tk.Frame(main_frame)
 			in_frame.grid(column=1, row=0)
 
-			tk.Label(in_frame, text="in.", font=self.myfont).pack(padx=6, ipadx=12)
+			tk.Label(in_frame, text="in.", font=self.myfont).pack(padx=6, ipadx=6)
 			inch = ttk.Combobox(in_frame, height=20, width=6, values=tuple(self.vals["in"]), font=self.myfont, state="readonly")
-			inch.pack(padx=6, pady=12, ipadx=12)
+			inch.pack(padx=6, pady=6, ipadx=6)
 			inch.set(0)
 			return inch
 
@@ -52,9 +55,9 @@ class Framing:
 			fr_frame = tk.Frame(main_frame)
 			fr_frame.grid(column=2, row=0, sticky="e")
 
-			tk.Label(fr_frame, text=bytes.fromhex("BE"), font=self.myfont).pack(padx=6, ipadx=12)
+			tk.Label(fr_frame, text=bytes.fromhex("BE"), font=self.myfont).pack(padx=6, ipadx=6)
 			fr = ttk.Combobox(fr_frame, height=20, width=6, values=("0", "1/16", "1/8", "3/16", "1/4", "5/16", "3/8", "7/16", "1/2", "9/16", "5/8", "11/16", "3/4", "13/16", "7/8", "15/16"), font=self.myfont, state="readonly")
-			fr.pack(padx=6, pady=12, ipadx=12)
+			fr.pack(padx=6, pady=6, ipadx=6)
 			fr.set(0)
 			return fr
 
@@ -73,13 +76,13 @@ class Framing:
 	
 	def buildNotebooks(self):
 		def mainNotebook(self):
-			self.main_notebook=ttk.Notebook(self.root)
+			self.main_notebook=ttk.Notebook(self.root, style='Custom.TNotebook')
 			self.main_notebook.pack(fill='both')
 			self.roof_tab=tk.Frame(self.main_notebook)
-			self.roof_tab.pack()
+			self.roof_tab.pack(pady=6, ipady=6)
 			self.main_notebook.add(self.roof_tab, text='Roof')
 			self.deck_tab=tk.Frame(self.main_notebook)
-			self.deck_tab.pack()
+			self.deck_tab.pack(pady=6, ipady=6)
 			self.main_notebook.add(self.deck_tab, text='Deck')
    
 		def subNotebook(self):
@@ -118,92 +121,15 @@ class Framing:
 
 	def buildRoofTab(self):
 		def buildRoofInput(self):
-			self.roof_width_input_frame=tk.Frame(self.roof_input_tab)
-			self.roof_width_input_frame.pack()
-			self.roof_depth_input_frame=tk.Frame(self.roof_input_tab)
-			self.roof_depth_input_frame.pack()
-			self.roof_width_lbl_frame = tk.Frame(self.roof_width_input_frame)
-			tk.Label(self.roof_width_lbl_frame, text = "Building width:", font = self.myfont).pack(side = 'top', padx=12, pady=1)
-			self.roof_width_lbl_frame.pack(side = 'top')
-			
-			self.roof_depth_lbl_frame = tk.Frame(self.roof_depth_input_frame)
-			tk.Label(self.roof_depth_lbl_frame, text = "Building depth:", font = self.myfont).pack(side = 'top', padx=12, pady=1)
-			self.roof_depth_lbl_frame.pack(side = 'top')
-		 	
-			def buildWidth(self):
-				self.roof_width_measurements_frame = tk.Frame(self.roof_width_input_frame)
-				def buildWFt(self):
-					ft_frame = tk.Frame(self.roof_width_measurements_frame)
-					ft_frame.grid(column = 0, row = 0, sticky = 'w')
-					tk.Label(ft_frame, text = "ft.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					ft = ttk.Combobox(ft_frame, height = 20, width = 6, values = tuple(self.vals['ft']), font = self.myfont, state = 'readonly')
-					ft.pack(padx = 6, pady=12, ipadx = 12)
-					ft.set(10)
-					
-				def buildWIn(self):
-					self.roof_width_in_frame = tk.Frame(self.roof_width_measurements_frame)
-					self.roof_width_in_frame.grid(column = 1, row = 0)
-					tk.Label(self.roof_width_in_frame, text = "in.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.roof_width_in = ttk.Combobox(self.roof_width_in_frame, height = 20, width = 6, values = tuple(self.vals['in']), font = self.myfont, state = 'readonly')
-					self.roof_width_in.pack(padx = 6, pady=12, ipadx = 12)
-					self.roof_width_in.set(0)
-					  
-				def buildWFr(self):
-					self.roof_width_fr_frame = tk.Frame(self.roof_width_measurements_frame)
-					self.roof_width_fr_frame.grid(column = 2, row = 0, sticky = 'e')
-					tk.Label(self.roof_width_fr_frame, text = bytes.fromhex('BE'), font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.roof_width_fr = ttk.Combobox(self.roof_width_fr_frame, height = 20, width = 6, values = ("0", "1/16", "1/8", "3/16", "1/4", "5/16", "3/8", "7/16", "1/2", "9/16", "5/8", "11/16", "3/4", "13/16", "7/8", "15/16"), font = self.myfont, state = 'readonly')
-					self.roof_width_fr.pack(padx = 6, pady=12, ipadx = 12)
-					self.roof_width_fr.set(0)
-
-				buildWFt(self)
-				buildWIn(self)
-				buildWFr(self)
-				
-				self.roof_width_measurements_frame.pack(side = 'top')
-				self.roof_width_input_frame.pack(fill = 'x')
-				self.buildSep(self.roof_width_input_frame)
-			# ttk.Separator(self.roof_width_input_frame, orient='horizontal').pack(fill='both', pady=12)
-  
-			def buildDepth(self):
-				self.roof_depth_measurements_frame = tk.Frame(self.roof_depth_input_frame)
-				
-				def buildDFt(self):
-					self.roof_depth_ft_frame = tk.Frame(self.roof_depth_measurements_frame)
-					self.roof_depth_ft_frame.grid(column = 0, row = 0, sticky = 'w')
-					tk.Label(self.roof_depth_ft_frame, text = "ft.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.roof_depth_ft = ttk.Combobox(self.roof_depth_ft_frame, height = 20, width = 6, values = tuple(self.vals['ft']), font = self.myfont, state = 'readonly')
-					self.roof_depth_ft.pack(padx = 6, pady=12, ipadx = 12)
-					self.roof_depth_ft.set(10)
-				def buildDIn(self):
-					self.roof_depth_in_frame = tk.Frame(self.roof_depth_measurements_frame)
-					self.roof_depth_in_frame.grid(column = 1, row = 0)
-					tk.Label(self.roof_depth_in_frame, text = "in.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.roof_depth_in = ttk.Combobox(self.roof_depth_in_frame, height = 20, width = 6, values = tuple(self.vals['in']), font = self.myfont, state = 'readonly')
-					self.roof_depth_in.pack(padx = 6, pady=12, ipadx = 12)
-					self.roof_depth_in.set(0)
-				def buildDFr(self):
-					self.roof_depth_fr_frame = tk.Frame(self.roof_depth_measurements_frame)
-					self.roof_depth_fr_frame.grid(column = 2, row = 0, sticky = 'e')
-					tk.Label(self.roof_depth_fr_frame, text = bytes.fromhex('BE'), font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.roof_depth_fr = ttk.Combobox(self.roof_depth_fr_frame, height = 20, width = 6, values = ("0", "1/16", "1/8", "3/16", "1/4", "5/16", "3/8", "7/16", "1/2", "9/16", "5/8", "11/16", "3/4", "13/16", "7/8", "15/16"), font = self.myfont, state = 'readonly')
-					self.roof_depth_fr.pack(padx = 6, pady=12, ipadx = 12)
-					self.roof_depth_fr.set(0)
-
-				buildDFt(self)
-				buildDIn(self)
-				buildDFr(self)
-				
-				self.roof_depth_measurements_frame.pack(side = 'top')
-				self.roof_depth_input_frame.pack(fill = 'x')
-				self.buildSep(self.roof_depth_input_frame)
-				# ttk.Separator(self.roof_depth_input_frame, orient='horizontal').pack(fill='both', pady=12)
+			self.buildSep(self.roof_input_tab)
+			self.roof_width=self.buildMeasurementInput(self.roof_input_tab, "Width of Building")
+			self.buildSep(self.roof_input_tab)
+			self.roof_depth=self.buildMeasurementInput(self.roof_input_tab, "Depth of Building")
+			self.buildSep(self.roof_input_tab)
 
 			def buildOther(self):
 				pass
 
-			buildWidth(self)
-			buildDepth(self)
 			buildOther(self)
  
 		def buildRoofData(self):
@@ -229,97 +155,22 @@ class Framing:
 
 	def buildDeckTab(self):
 		def buildDeckInput(self):
-			self.deck_width_input_frame=tk.Frame(self.deck_input_tab)
-			self.deck_width_input_frame.pack()
-			self.deck_length_input_frame=tk.Frame(self.deck_input_tab)
-			self.deck_length_input_frame.pack()
-			self.deck_width_lbl_frame = tk.Frame(self.deck_width_input_frame)
-			tk.Label(self.deck_width_lbl_frame, text = "Building width:", font = self.myfont).pack(side = 'top', padx=12, pady=1)
-			self.deck_width_lbl_frame.pack(side = 'top')
+			self.buildSep(self.deck_input_tab)
+			self.deck_width=self.buildMeasurementInput(self.deck_input_tab, "Width of Deck [across building]")
+			self.buildSep(self.deck_input_tab)
+			self.deck_length=self.buildMeasurementInput(self.deck_input_tab, "Length of Deck [from building]")
+			self.buildSep(self.deck_input_tab)
 			
-			self.deck_length_lbl_frame = tk.Frame(self.deck_length_input_frame)
-			tk.Label(self.deck_length_lbl_frame, text = "Building length:", font = self.myfont).pack(side = 'top', padx=12, pady=1)
-			self.deck_length_lbl_frame.pack(side = 'top')
-		 	
-			def buildWidth(self):
-				self.deck_width_measurements_frame = tk.Frame(self.deck_width_input_frame)
-				def buildWFt(self):
-					ft_frame = tk.Frame(self.deck_width_measurements_frame)
-					ft_frame.grid(column = 0, row = 0, sticky = 'w')
-					tk.Label(ft_frame, text = "ft.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					ft = ttk.Combobox(ft_frame, height = 20, width = 6, values = tuple(self.vals['ft']), font = self.myfont, state = 'readonly')
-					ft.pack(padx = 6, pady=12, ipadx = 12)
-					ft.set(10)
-					
-				def buildWIn(self):
-					self.deck_width_in_frame = tk.Frame(self.deck_width_measurements_frame)
-					self.deck_width_in_frame.grid(column = 1, row = 0)
-					tk.Label(self.deck_width_in_frame, text = "in.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.deck_width_in = ttk.Combobox(self.deck_width_in_frame, height = 20, width = 6, values = tuple(self.vals['in']), font = self.myfont, state = 'readonly')
-					self.deck_width_in.pack(padx = 6, pady=12, ipadx = 12)
-					self.deck_width_in.set(0)
-					  
-				def buildWFr(self):
-					self.deck_width_fr_frame = tk.Frame(self.deck_width_measurements_frame)
-					self.deck_width_fr_frame.grid(column = 2, row = 0, sticky = 'e')
-					tk.Label(self.deck_width_fr_frame, text = bytes.fromhex('BE'), font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.deck_width_fr = ttk.Combobox(self.deck_width_fr_frame, height = 20, width = 6, values = ("0", "1/16", "1/8", "3/16", "1/4", "5/16", "3/8", "7/16", "1/2", "9/16", "5/8", "11/16", "3/4", "13/16", "7/8", "15/16"), font = self.myfont, state = 'readonly')
-					self.deck_width_fr.pack(padx = 6, pady=12, ipadx = 12)
-					self.deck_width_fr.set(0)
-
-				buildWFt(self)
-				buildWIn(self)
-				buildWFr(self)
-				
-				self.deck_width_measurements_frame.pack(side = 'top')
-				self.deck_width_input_frame.pack(fill = 'x')
-				self.buildSep(self.deck_width_input_frame)
-			# ttk.Separator(self.deck_width_input_frame, orient='horizontal').pack(fill='both', pady=12)
-  
-			def buildLength(self):
-				self.deck_length_measurements_frame = tk.Frame(self.deck_length_input_frame)
-				
-				def buildDFt(self):
-					self.deck_length_ft_frame = tk.Frame(self.deck_length_measurements_frame)
-					self.deck_length_ft_frame.grid(column = 0, row = 0, sticky = 'w')
-					tk.Label(self.deck_length_ft_frame, text = "ft.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.deck_length_ft = ttk.Combobox(self.deck_length_ft_frame, height = 20, width = 6, values = tuple(self.vals['ft']), font = self.myfont, state = 'readonly')
-					self.deck_length_ft.pack(padx = 6, pady=12, ipadx = 12)
-					self.deck_length_ft.set(10)
-				def buildDIn(self):
-					self.deck_length_in_frame = tk.Frame(self.deck_length_measurements_frame)
-					self.deck_length_in_frame.grid(column = 1, row = 0)
-					tk.Label(self.deck_length_in_frame, text = "in.", font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.deck_length_in = ttk.Combobox(self.deck_length_in_frame, height = 20, width = 6, values = tuple(self.vals['in']), font = self.myfont, state = 'readonly')
-					self.deck_length_in.pack(padx = 6, pady=12, ipadx = 12)
-					self.deck_length_in.set(0)
-				def buildDFr(self):
-					self.deck_length_fr_frame = tk.Frame(self.deck_length_measurements_frame)
-					self.deck_length_fr_frame.grid(column = 2, row = 0, sticky = 'e')
-					tk.Label(self.deck_length_fr_frame, text = bytes.fromhex('BE'), font = self.myfont).pack(padx = 6, ipadx = 12)
-					self.deck_length_fr = ttk.Combobox(self.deck_length_fr_frame, height = 20, width = 6, values = ("0", "1/16", "1/8", "3/16", "1/4", "5/16", "3/8", "7/16", "1/2", "9/16", "5/8", "11/16", "3/4", "13/16", "7/8", "15/16"), font = self.myfont, state = 'readonly')
-					self.deck_length_fr.pack(padx = 6, pady=12, ipadx = 12)
-					self.deck_length_fr.set(0)
-
-				buildDFt(self)
-				buildDIn(self)
-				buildDFr(self)
-				self.deck_length_measurements_frame.pack(side = 'top')
-				self.deck_length_input_frame.pack(fill = 'x')
-				ttk.Separator(self.deck_length_input_frame, orient='horizontal').pack(fill='both', pady=12)
 			
 			def buildOther(self):
 				self.deck_other_frame = tk.Frame(self.deck_input_tab)
-				decking=self.buildMeasurementInput(self.deck_other_frame, "Material width:", False, True, True)
-				overhang=self.buildMeasurementInput(self.deck_other_frame, "Overhang:", False, True, True)
-				gap=self.buildMeasurementInput(self.deck_other_frame, "Gap:", False, False, True)
-				self.deck_decking_in, self.deck_decking_fr, self.deck_overhang_in, self.deck_overhang_fr, self.deck_gap_fr=decking['in'], decking['fr'], overhang['in'], overhang['fr'], gap['fr']
+				self.decking=self.buildMeasurementInput(self.deck_other_frame, "Material width:", False, True, True)
+				self.overhang=self.buildMeasurementInput(self.deck_other_frame, "Overhang:", False, True, True)
+				self.gap=self.buildMeasurementInput(self.deck_other_frame, "Gap:", False, False, True)
 				self.deck_other_frame.pack(fill='x', padx=12)
 				self.deck_go_btn = tk.Button(self.deck_input_tab, text = "GO", command = self.deckClicked)
 				self.deck_go_btn.pack
 			
-			buildWidth(self)
-			buildLength(self)
 			buildOther(self)
  
 		def buildDeckData(self):
